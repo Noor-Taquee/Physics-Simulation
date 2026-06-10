1df6642cd3939bff5f3416ec4a7e15f556f09b01
Noor Taquee <noortaquee001@gmail.com>
2026-06-10 15:03:07 +0530

    Added test command and test fix

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,21 @@
-from logic import Body, mainloop, simState
+from src.pygame.core.logic import Environment, PhysicsBody
 
-# 1. Define a callback
-def log_position(body: Body):
-    print(f"{body.name} position: ({body.position.x:.2f}, {body.position.y:.2f})")
+
+def log_position(body: PhysicsBody):
+  print(f"{body.name} position: ({body.position.x:.2f}, {body.position.y:.2f})")
+
+
+space = Environment()
 
 # 2. Setup bodies
-body1 = Body("name-1", 1e15, 100, 50) # Increased mass to see movement
+body1 = PhysicsBody("name-1", 1e15, 100, 50)
 body1.add_event_listener("update", log_position)
 
-body2 = Body("name-2", 1e15, 200, 78)
+body2 = PhysicsBody("name-2", 1e15, 200, 78)
 body2.add_event_listener("update", log_position)
-
-# 3. Add them to the state and Flip the switch
-simState.bodies = [body1, body2]
-simState.flowSwitch = True
+space.register(body1, body2)
 
 # 4. Run simulation
 print("Starting simulation...")
 for i in range(500):
-    mainloop(0.01) # Using a slightly larger dt to see results faster
+  space.calculate(1)
